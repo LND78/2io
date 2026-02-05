65fd49b060b55984f26c6606cc6a0fa08e2b2c63
NamanSoni78 <NamanSoni78@outlook.com>
2026-02-05 19:49:18 +0000

    tyuh

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleAnswer(id) {
-    const answerDiv = document.getElementById(id);
-    if (answerDiv.classList.contains('hidden')) {
-        answerDiv.classList.remove('hidden');
-    } else {
-        answerDiv.classList.add('hidden');
-    }
-}
